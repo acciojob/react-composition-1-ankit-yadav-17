3ee9a07855906e58ee363ef0dea45f5c1a5926ec
ankit-yadav-17 <119732393+ankit-yadav-17@users.noreply.github.com>
2023-07-15 05:17:10 +0000

    react ques

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
+import Tab from "./Tab"
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Tab/>
     </div>
   )
 }
---
+++ src/components/Tab.js
@@ -0,0 +1,29 @@
+import React,{useState} from "react"
+const tabs = [
+    {title:"Tab 1",content:"This is the content for Tab 1."},
+    {title:"Tab 2",content:"This is the content for Tab 2."},
+    {title:"Tab 3",content:"This is the content for Tab 3."},
+]
+
+const Tab = () =>{
+    
+    const [activeTab,setActiveTab] = useState(0);
+
+    const handleClick = (index) =>{
+        setActiveTab(index);
+    }
+
+    return(
+        <div>
+            <ul>
+                {tabs.map((tab,index) => (
+                    <li onClick ={() => handleClick(index)}>{tab.title}</li>
+                ))}
+            </ul>
+
+            <div>{tabs[activeTab].content}</div>
+        </div>
+    )
+}
+
+export default Tab;
